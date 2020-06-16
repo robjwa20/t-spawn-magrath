d15e240fd7a7da5ff6d6330682eccbdd01e308c2
Robjwa <robert.j.w.allen@gmail.com>
2020-06-16 19:16:34 +0100

    Leaderboard added to url

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 module.exports = {
   siteMetadata: {
-    title: `Creative Portfolio`,
+    title: `BlockMaster`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
